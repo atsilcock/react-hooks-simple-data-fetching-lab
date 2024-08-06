c96b2b9948d9498c8f92db6e51a72fa92892fecd
atsilcock <143049271+atsilcock@users.noreply.github.com>
2024-08-06 16:14:27 -0600

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, {useState, useEffect} from 'react'
+
+
+function App() {
+const [holdState, setHoldState] = useState([])
+const [isLoader, setIsLoader] = useState(false)
+console.log(isLoader)
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response => response.json()))
+    .then((data) => {
+        setHoldState(data.message)
+        setIsLoader(true)
+     })
+}, [])
+
+if(!isLoader) return <p>Loading...</p>
+
+  return (
+    <div>
+      <img src={holdState} alt="A Random Dog" />
+    </div>
+  )
+}
+
+
+export default App 
